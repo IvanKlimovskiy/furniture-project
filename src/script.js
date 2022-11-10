ad7131c8d10a87294992126b2d678e6e6f72de18
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2022-11-10 23:26:03 +0300

    Add contact and popup

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const popup = document.querySelector('.popup');
+const imageLink = document.querySelector('.furniture__image-link');
+const buttonClosePopup = document.querySelector('.popup__close-button');
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+buttonClosePopup.addEventListener("click", () => {
+  closePopup(popup);
+});
+
+imageLink.addEventListener("click", () => {
+  openPopup(popup);
+});
