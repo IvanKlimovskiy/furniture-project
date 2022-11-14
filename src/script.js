c41920b009b257cd4dc5725c9e0b545a9e42976a
IvanKlimovskiy <111538629+IvanKlimovskiy@users.noreply.github.com>
2022-11-14 14:56:38 +0300

    Добавить работающее меню для главной страницы

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 const popup = document.querySelector('.popup');
 const imageLink = document.querySelector('.furniture__image-link');
 const buttonClosePopup = document.querySelector('.popup__close-button');
-const width = screen.width;
+const buttonBurgerMenu = document.querySelector('.header__burger-menu'); 
+const navigationMenu = document.querySelector('.nav-menu');
+const buttonCloseNavigationMenu = document.querySelector('.header__close-menu-button');
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -18,3 +20,11 @@ buttonClosePopup.addEventListener("click", () => {
 imageLink.addEventListener("click", () => {
   openPopup(popup);
 });
+
+buttonBurgerMenu.addEventListener('click', () => {
+  navigationMenu.style.left = '0';
+})
+
+buttonCloseNavigationMenu.addEventListener('click', () => {
+  navigationMenu.style.left = '-100%';
+})
