3c4a65a92726151d25b5a41c5b01092c3f679296
IvanKlimovskiy <111538629+IvanKlimovskiy@users.noreply.github.com>
2022-11-17 20:59:55 +0300

    Изменить попап и добавить функционал (кнопку закрытия поменять надо)

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
 const popup = document.querySelector('.popup');
-const imageLink = document.querySelector('.furniture__image-link');
+const imagesLink = document.querySelectorAll('.furniture__image-link');
 const buttonClosePopup = document.querySelector('.popup__close-button');
 const buttonBurgerMenu = document.querySelector('.header__burger-menu'); 
 const navigationMenu = document.querySelector('.nav-menu');
 const buttonCloseNavigationMenu = document.querySelector('.header__close-menu-button');
+const popupOpenedImage = document.querySelector('.popup__image')
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -13,13 +14,9 @@ function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-// buttonClosePopup.addEventListener("click", () => {
-//   closePopup(popup);
-// });
-
-// imageLink.addEventListener("click", () => {
-//   openPopup(popup);
-// });
+buttonClosePopup.addEventListener("click", () => {
+  closePopup(popup);
+});
 
 buttonBurgerMenu.addEventListener('click', () => {
   navigationMenu.style.left = '0';
@@ -27,4 +24,11 @@ buttonBurgerMenu.addEventListener('click', () => {
 
 buttonCloseNavigationMenu.addEventListener('click', () => {
   navigationMenu.style.left = '-100%';
+})
+
+Array.from(imagesLink).forEach((el) => {
+  el.addEventListener('click', function(evt) {
+    popupOpenedImage.src = evt.target.src;
+    openPopup(popup);
+  })
 })
