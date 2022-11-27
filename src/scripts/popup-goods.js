2039a42196af7c86d87679f53c86f6cabac5647d
IvanKlimovskiy <klimoffsky2707@yandex.ru>
2022-11-27 16:52:48 +0300

    Подключить препроцессор и всё перенести в него, плюс переименовать некоторые классы

---
+++ src/scripts/popup-goods.js
@@ -0,0 +1,23 @@
+const popup = document.querySelector('.popup');
+const goods = document.querySelectorAll('.goods__image-wrapper');
+const buttonClosePopup = document.querySelector('.popup__close-button');
+const popupOpenedImage = document.querySelector('.popup__image')
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+buttonClosePopup.addEventListener("click", () => {
+  closePopup(popup);
+});
+
+Array.from(goods).forEach((el) => {
+  el.addEventListener('click', function(evt) {
+    popupOpenedImage.src = evt.target.src;
+    openPopup(popup);
+  })
+})
