a0fb8916a79e2f3feabc479f243fea429fdd67a9
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2023-01-14 04:37:27 +0300

    Добавить валидацию формы

--- src/scripts/popup-goods.js
+++ src/scripts/popup-goods.js
@@ -3,12 +3,21 @@ const goods = Array.from(document.querySelectorAll(".goods__image-wrapper"));
 const buttonClosePopup = document.querySelector(".popup__close-button");
 const popupOpenedImage = document.querySelector(".popup__image");
 
+const closeByEscape = (evt) => {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector('.popup_opened')
+    closePopup(openedPopup)
+  }
+}
+
 function openPopup(popup) {
   popup.classList.add("popup_opened");
+  window.addEventListener("keydown", closeByEscape)
 }
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
+  window.removeEventListener("keydown", closeByEscape)
 }
 
 buttonClosePopup.addEventListener("click", () => {
